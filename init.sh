e14f09efd058aa5475bbf2511ba25fc048234070
Jonmichael Chambers <jon@tyrbo.net>
2016-12-29 13:16:12 -0700

    Delete init.sh

--- init.sh
+++
@@ -1,8 +0,0 @@
-sudo apt-get install -y software-properties-common
-sudo apt-add-repository -y ppa:ansible/ansible
-sudo apt-get update
-sudo apt-get install -y ansible
-
-cd $HOME
-curl https://raw.githubusercontent.com/tyrbo/dotfiles/master/playbook.yml > playbook.yml
-ansible-playbook --ask-become-pass playbook.yml
